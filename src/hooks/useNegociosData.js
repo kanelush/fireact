006b93a6c869f3b1563d4d816934f635ab6be2eb
kanelush <kanelushi@protonmail.com>
2022-03-18 02:00:57 -0300

    first commit

---
+++ src/hooks/useNegociosData.js
@@ -0,0 +1,17 @@
+import { useQuery } from 'react-query'
+import axios from 'axios'
+
+
+const fetchNegocios = () => {
+    return axios.get('http://127.0.0.1:8000/api/negocios')
+  }
+
+export const useNegociosData = (onSuccess, onError) => {
+  return useQuery('negocios', fetchNegocios, {
+    onSuccess,
+    onError
+  })
+  
+  
+  
+}
